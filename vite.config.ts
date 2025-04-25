d812e8c3c96d8e67322d6701fd14c4f68a3defaa
Boonyarit Pornaul <144136018+boonyaritpornuan@users.noreply.github.com>
2025-04-25 08:59:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/SkyBirdAdventure3D/',
+base:'/SkyBirdAdventure3D/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
